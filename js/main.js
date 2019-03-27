17937c3f0a1af77deab0a5120b70627124440d7c
Mallory Coleman <mallorycoleman@Mallorys-MacBook-Air.local>
2019-03-26 20:19:30 -0400

    added draggable to js

--- js/main.js
+++ js/main.js
@@ -1,45 +1,56 @@
-(() => {
-   //create a variable stack and grab all of the dom elements from the page
-   let aud = document.querySelector('audio'),
-       tracks = document.querySelectorAll('.trackholder');
+// JavaScript Document
+//Self Executing Anonymous Function - use this. Prevents Global Variables
 
-
-  // function() {
-  //   var play = document.getElementById("audio");
-  //   play.addEventListener("dragover", function (ev) {
-  //       ev.preventDefault();
-  //   }, false);
-  //   play.addEventListener("drop", function (ev) {
-  //       ev.preventDefault();
-  //       var data = ev.dataTransfer.getData("audio/dance.mp3");
-
-
-  // write the functions for the audio element
-  function playAudio() {
-  	aud.play();
+(function() {
+  //console.log("Yep the SEAF Fired");
+  
+  //Variables
+  //save referene to dropzone in DOM
+  let dropZone = document.querySelector(".dancing1");
+  //save referene to dragable element in DOM
+  let dragElement = document.querySelector(".buttons div");
+  //create variable tunes save new Audio object instance inside of it
+  let tunes = new Audio("dance.mp3");
+  
+  //functions
+  function startDrag (e) {
+    //we are capturing the event object and using it to set some information about what is being dragged
+    //in this case we are saying that we are storing some text and it is the id of element being moved
+    //We need to set this, this iswhy it wasn't working earlier
+    //Set the drag's format and data. Use the event target's id for the data
+    e.dataTransfer.setData('text', e.target.id);
+    console.log("dragging");
+    console.log(e.dataTransfer.getData('text'));
+   }
+    
+  function endDrag() {
+    console.log("Stopped Dragging");
+   }
+    
+  function overDrag(e) {
+    console.log("draggedOver");
+    //capture event object and prevent the default behaviour so that draggedOver event can properly be observed
+    e.preventDefault();
   }
-
-  function pauseAudio() {
-  	aud.pause(true);
+    
+  function dropped(e) {
+    console.log("dropped");
+    //capture event object and prevent the default behaviour so that draggedOver event can properly be observed
+    e.preventDefault();
+    //This is where you might play a sound
+    tunes.play();
   }
+  
+  
 
-  function rewindAudio() {
-  	aud.currentTime = 0;
-  }
-
-
-  function swapSource() {
-  	let currentTrack = this.dataset.currenttrack;
-
-  	aud.src = `audio/${currentTrack}`;
-  	aud.load();
-  	aud.play();
-  }
-
-  // add event handling
-
-
-  tracks.forEach(track => track.addEventListener("click", swapSource));
-
-
+  //Event Listener for when the drag interaction starts.
+  dragElement.addEventListener('dragstart', startDrag); 
+  //Event Listener for when the drag interaction finishes.
+  dragElement.addEventListener('dragend', endDrag);
+  // Event Listener for when the dragged element is over the drop zone.
+  dropZone.addEventListener('dragover', overDrag);    
+  // Event Listener for when the dragged element dropped in the drop zone.
+  dropZone.addEventListener('drop', dropped);
+  
+  
 })();
